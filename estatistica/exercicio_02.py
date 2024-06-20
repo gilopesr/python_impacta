0ca46c868400bd9f2650f8b2f176b9d57290443a
Giovana Lopes <165718041+gilopesr@users.noreply.github.com>
2024-06-19 22:29:57 -0300

    Create exercicio_02.py

---
+++ estatistica/exercicio_02.py
@@ -0,0 +1,13 @@
+# Exercicio 2
+# pergunte o tamanho da amostra
+# calcule a Média e o Desvio Padrão da amostra
+
+lista =[]
+n = int(input("Qual o tamanho da amostra? "))
+for i in range(n):
+  numero = float(input("informe o número "))
+  lista.append(numero)
+media = np.median(lista)
+DP = np.std(lista)
+print(f"media = {media}")
+print(f"DP = {DP:.2f}")
