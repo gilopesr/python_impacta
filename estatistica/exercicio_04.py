2b414dc9676253a02ab1713387d85bb7ffb275ca
Giovana Lopes <165718041+gilopesr@users.noreply.github.com>
2024-08-09 10:32:46 -0300

    Create exercicio_04.py

---
+++ estatistica/exercicio_04.py
@@ -0,0 +1,29 @@
+#Faça um programa que receba a temperatura media
+#de cada mes do ano e armazene-as em uma lista.
+#Em seguida, calcule a media e o desvio pardrao das temperaturas
+#imprima o desvio padrao
+# Determine um intervalo [media - DP , media + DP]
+# conte quantas temperaturas estão acima, abaixo e dentro do interval
+import numpy as np
+import statistics as sta
+
+lista = []
+acima = []
+abaixo = []
+for i in range (12):
+ temp = float(input(f"insira a temperatura media do {i+1}mês"))
+ lista.append(temp)
+ media = np.median(lista)
+ DP = np.std(lista)
+ print(f"media = {media}")
+ print(f"DP = {DP}")
+ menos= media - DP
+ mais= media + DP
+for i in range(12):
+  if temp >= mais
+
+
+
+
+
+
