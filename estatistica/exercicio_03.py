99a95f044d17cd0c4c44b643e739dd0dffe0ac44
Giovana Lopes <165718041+gilopesr@users.noreply.github.com>
2024-08-09 10:31:26 -0300

    Create exercicio_03.py

---
+++ estatistica/exercicio_03.py
@@ -0,0 +1,14 @@
+# Exercicio 3
+# resolva usando uma lista
+# Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
+
+import numpy as np
+import statistics as sta
+
+lista =[]
+for i in range(4):
+  nota = float(input("Insira a nota "))
+  lista.append(nota)
+media = np.median(lista)
+print(f"Notas = {lista}")
+print(f"Média = {media:.1f}")
