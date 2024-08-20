1a455bc43d66f9e6f8ca3c1d34a9aebed3d7f69c
Giovana Lopes <165718041+gilopesr@users.noreply.github.com>
2024-08-20 10:09:39 -0300

    Delete POO directory

--- POO/aula_1308/ex02.py
+++
@@ -1,7 +0,0 @@
-#Exercício 2: Cálculo de Médias Ponderadas
-# Desenvolva uma função chamada calcular_medias que receba uma lista de
-# listas, onde cada sublista contém três notas de um aluno. A função deve
-# calcular a média ponderada de cada aluno, considerando pesos de 30%
-# para as duas primeiras notas e 40% para a terceira nota.
-# Retorne uma lista com as médias calculadas e imprima as notas dos alunos
-# junto com as respectivas médias.
