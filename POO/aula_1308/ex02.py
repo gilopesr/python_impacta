660e929d1647f024aef3cb1e347ec1f339070699
Giovana Lopes <165718041+gilopesr@users.noreply.github.com>
2024-08-20 16:19:01 -0300

    Update ex02.py

--- POO/aula_1308/ex02.py
+++ POO/aula_1308/ex02.py
@@ -5,3 +5,27 @@
 # para as duas primeiras notas e 40% para a terceira nota.
 # Retorne uma lista com as médias calculadas e imprima as notas dos alunos
 # junto com as respectivas médias.
+
+def calcular_medias(lista_notas):
+
+    medias = []
+    
+    for notas in lista_notas:
+        n1, n2, n3 = notas
+
+        media_ponderada = (n1 * 0.30) + (n2 * 0.30) + (n3 * 0.40)
+
+        medias.append(media_ponderada)
+        
+        print(f'Notas: {notas} => Média Ponderada: {media_ponderada:.2f}')
+    
+    return medias
+
+
+lista_notas = [
+    [5.5, 8.5, 9.5],
+    [2.5, 6.0, 7.5],
+    [9.0, 3.0, 6.5]
+]
+
+medias = calcular_medias(lista_notas)
