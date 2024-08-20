62e1f4daf408d1eeb2659acf6b48d2a515d5b6e3
Giovana Lopes <165718041+gilopesr@users.noreply.github.com>
2024-08-20 16:47:22 -0300

    Update ex04.py

--- POO/aula_2008/ex04.py
+++ POO/aula_2008/ex04.py
@@ -5,3 +5,24 @@
 # [4, 5, 6]
 # Para estas listas, a função deve retornar:
 # [1, 4, 2, 5, 3, 6]
+
+def intercala_numeros(lista1, lista2):
+    lista = []
+    for i in range(len(lista1)):
+        lista.append(lista1[i])
+        lista.append(lista2[i])
+    return lista
+
+lista1 = []
+lista2 = []
+
+for i in range (3):
+    num = int(input('insira os numeros da lista 1: '))
+    lista1.append(num)
+
+for i in range (3):
+    num = int(input('insira os numeros da lista 2: '))
+    lista2.append(num)
+
+
+print(intercala_numeros(lista1,lista2))
