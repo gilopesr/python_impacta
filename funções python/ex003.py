846708b822deb5b7769cca37e34cb3228b8b62ad
Giovana Lopes <165718041+gilopesr@users.noreply.github.com>
2024-08-29 20:31:31 -0300

    Create ex003.py

---
+++ funções python/ex003.py
@@ -0,0 +1,8 @@
+# 03) criar uma função que  mostra linha ml()
+def ml ():
+  print('˗ˏˋ ♡ ˎˊ˗' * 6)
+ml()
+print('         Aula de Linguagem de Programação')
+ml()
+print('                 ALUNA GIOVANA')
+ml()
