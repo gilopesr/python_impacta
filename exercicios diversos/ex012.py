34029d474a1fdcdc77a69c1fa9b5b84d60a2fc48
Giovana Lopes <165718041+gilopesr@users.noreply.github.com>
2024-08-29 20:16:59 -0300

    Create ex012.py

---
+++ exercicios diversos/ex012.py
@@ -0,0 +1,15 @@
+#12) Supondo que a população de um país A seja da ordem de 80000 habitantes
+#com uma taxa anual de crescimento de 3% e que a população de B seja 200000
+#habitantes com uma taxa de crescimento de 1.5%. Faça um programa que calcule
+#e escreva o número de anos necessários para que a população do país A
+#ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
+
+PopA = 80000
+PopB = 200000
+cont = 0
+while PopA <= PopB:
+  PopA += (PopA * 0.03)
+  PopB += (PopB * 0.015)
+  cont += 1
+
+print(PopA, PopB, cont)
