c8d815b3fc2aa0af45a898ae8859d86ae10e6e80
Giovana Lopes <165718041+gilopesr@users.noreply.github.com>
2024-08-29 20:22:09 -0300

    Create ex016.py

---
+++ exercicios diversos/ex016.py
@@ -0,0 +1,16 @@
+# 19) Desenvolva um programa que apresente a soma dos valores pares
+# e dos valores ímpares da lista [21, 5, 34, 8, 16, 7, 3, 24, 36, 21, 75, 12]
+# nome:
+
+lista = [21, 5, 34, 8, 16, 7, 3, 24, 36, 21, 75, 12]
+i = 0
+par = 0
+impar = 0
+while i < 12:
+  if lista[i] % 2 == 0:
+    par = par + lista[i]
+    i += 1
+  else:
+    impar = impar + lista[i]
+    i+=1
+print(f'soma dos numeros pares = {par}\n soma dos numeros impares = {impar}')
