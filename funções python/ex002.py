cc825cce9ecc67c80325696d7dd99b1a6e04c106
Giovana Lopes <165718041+gilopesr@users.noreply.github.com>
2024-08-29 20:29:50 -0300

    Create ex002.py

---
+++ funções python/ex002.py
@@ -0,0 +1,9 @@
+# 02)
+
+def soma(num1, num2):
+  soma = num1 + num2
+  return print(f'{n1} + {n2}= {soma:.1f}')
+
+n1 = float(input('digite o 1° número: '))
+n2 = float(input('digite o 2° número: '))
+soma(n1,n2)
